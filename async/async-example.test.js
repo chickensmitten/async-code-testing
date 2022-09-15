985ae5741d4ca3837ff608b93953e436460fa6c5
sheng yeong <chia.shengyeong@gmail.com>
2022-09-15 11:02:46 +0800

    using .resolves or async await to test for promises

--- async/async-example.test.js
+++ async/async-example.test.js
@@ -1,11 +1,33 @@
 import { describe, expect, it } from "vitest";
-import { generateToken } from "./async-example";
+import { generateToken, generateTokenPromise } from "./async-example";
+
+it("should generate a token value", (done) => {
+  const testUserEmail = "test@test.com";
+
+  generateToken(testUserEmail, (err, token) => {
+    
+    try {
+      expect(token).toBeDefined(); // correct code
+      done();
+      // without try and with done(err); wrong async assertions will have timeout errors
+      // expect(token).toBe(2); // this is wrong but the test passes
+    } catch (err) {
+      done(err);
+    }
+    
+  });
+});
 
 it("should generate a token value", () => {
   const testUserEmail = "test@test.com";
 
-  generateToken(testUserEmail, (err, token) => {
-    // expect(token).toBeDefined();
-    expect(token).toBe(2); // this is wrong but the test passes
-  });
+  expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+});
+
+it("should generate a token value", async () => {
+  const testUserEmail = "test@test.com";
+
+  const token = await generateTokenPromise(testUserEmail);
+
+  expect(token).toBeDefined();
 });
